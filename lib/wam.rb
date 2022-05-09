7e5b9fccfea9a998e03f710b9643707a2f702761
Raels Koder <raels@uniblab.com>
2022-05-09 12:41:36 -0700

    Initial commit

---
+++ lib/wam.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "wam/version"
+
+module Wam
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/wam/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Wam
+  VERSION = "0.1.0"
+end
